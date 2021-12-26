b7b9826c18013b14a4344fa40d4db9803f6ce7b0
M. Abdullah Riaz <abdullah.riaz82@gmail.com>
2021-12-26 21:08:32 +0500

    updation

--- Google Search Analysis.R
+++ Google Search Analysis.R
@@ -2,12 +2,12 @@ library(gtrendsR)
 library(tidyverse)
 
 trends <- gtrends(keyword = c("Machine Learning"),time = "now 1-d")
-iot2020 <- trends$interest_by_country
 
 ibc1d<-trends$interest_by_country
-df[!is.na(df$hits),]
-ibc1d2<-df[!is.na(df$hits),]
-first_10_rows<-head(ibc1d2,10)
+df<- data.frame(ibc1d)
+ibc1d2<-df[!is.na(ibc1d$hits),] #removing NA values W.R.t Hits
 
+
+first_10_rows<-head(ibc1d2,10)
 barplot(first_10_rows$hits,names.arg=first_10_rows$location,xlab="Country Names",ylab="No. Of Machine Learning Hits",col="blue",
         main="Search Query Bar chart (Machine Learning)")
